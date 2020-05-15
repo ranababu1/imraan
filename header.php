63e48d726e16918de411b8709556262c521d424b
ranababu1 <imurokks@gmail.com>
2020-05-15 15:50:47 +0530

    adding favicon

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="shortcut icon" type="image/png" href="/wp-content/themes/twentytwenty/fav.png"/>
+	<link rel="shortcut icon" type="image/png" href="/wp-content/themes/imran2/fav.png"/>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
