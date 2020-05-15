53e2e398382c357582ef811b922696d16aa315b6
ranababu1 <imurokks@gmail.com>
2020-05-15 15:46:50 +0530

    adding favicon

--- header.php
+++ header.php
@@ -9,6 +9,9 @@
  * @since 1.0.0
  */
 ?>
+<?php
+$folder = get_template_directory();
+?>
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
@@ -16,7 +19,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="shortcut icon" type="image/png" href="/fav.png"/>
+	<link rel="shortcut icon" type="image/png" href="<?php echo $folder.'/fav.png'?>"/>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
